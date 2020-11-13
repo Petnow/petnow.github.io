138096259ecd01491f9ebf3bee6b6ad61f5d510e
Daehyun Pak <daehyun.pak@petnow.io>
2020-11-13 20:35:45 +0900

    20200726 홈페이지 리뉴얼

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    this.document.getElementById('btnAppStore').addEventListener('click', function() {
+        location.href = 'https://apps.apple.com/us/app/%ED%8E%AB%EB%82%98%EC%9A%B0/id1504831008?l=ko&ls=1';
+    });
+    this.document.getElementById('btnPlayStore').addEventListener('click', function() {
+        location.href = 'https://play.google.com/store/apps/details?id=io.petnow.id';
+    });
+}
